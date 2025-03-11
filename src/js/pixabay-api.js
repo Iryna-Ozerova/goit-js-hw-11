a859dd0c13e5d680e847862879189e98fa78a3ca
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-03-11 19:21:17 +0200

    hw is ready

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,24 @@
-//функція яка робить http запит на бек
+// http запит на бек
+import axios from 'axios';
+
+const API_KEY = '49186769-dabefba962826c776b77806d1';
+const BASE_URL = 'https://pixabay.com/api/';
+
+// Функція для отримання зображень
+export function searchImages(query) {
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data.hits)
+    .catch(error => {
+      console.error('Error fetching images:', error.message);
+      return [];
+    });
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,41 @@
-//import fetch from './js/pixabay-api.js';
-//import create gallery './js/render-function.js';
+import { searchImages } from './js/pixabay-api.js';
+import { updateGallery, showNoResultsMessage, showLoader, hideLoader } from './js/render-function.js';
+
+
+  const form = document.querySelector('.form-search');
+  const input = document.querySelector('.input-search');
+  const gallery = document.querySelector('.gallery');
+
+   form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const query = input.value.trim();
+    if (query === '') {
+      showNoResultsMessage('Please enter a search query!');
+      return;
+    }
+
+    gallery.innerHTML = ''; // Очищаємо галерею перед новим пошуком
+    showLoader(); // Показуємо лоадер перед запитом
+
+    searchImages(query)
+      .then(images => {
+        hideLoader(); // Ховаємо лоадер після отримання результатів
+
+        if (images.length === 0) {
+          showNoResultsMessage('Sorry, there are no images matching your search query. Please try again!');
+          return;
+        }
+
+        updateGallery(images);
+      })
+      .catch(error => {
+        hideLoader();
+        showNoResultsMessage('Error fetching images. Please try again!');
+        console.error('Помилка сервера:', error.message);
+      });
+
+    form.reset(); // Очистити поле введення після пошуку
+  });
+    
+
