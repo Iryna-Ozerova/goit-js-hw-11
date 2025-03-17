9ddb4620ce4bf0a3b7af73ecd8de3ce7665b217b
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-03-17 10:03:43 +0200

    upd loader

--- src/main.js
+++ src/main.js
@@ -1,36 +1,27 @@
 import { searchImages } from './js/pixabay-api.js';
 import { updateGallery, showNoResultsMessage } from './js/render-functions.js';
-import 'loaders.css/loaders.min.css';
-
 const form = document.querySelector('.form');
 const input = document.querySelector('.input-search');
 const gallery = document.querySelector('.gallery');
-const loader = document.querySelector('.loader'); 
-
+const loader = document.querySelector('.loader');
 // лоадер спочатку прихований
 loader.style.display = 'none';
-
 form.addEventListener('submit', function (event) {
   event.preventDefault();
-
   const query = input.value.trim();
   if (query === '') {
     showNoResultsMessage('Please enter a search query!');
     return;
   }
-
-  gallery.innerHTML = ''; // Очищаємо галерею перед новим пошуком
-  loader.style.display = 'block'; // Показуємо лоадер
-
+  gallery.innerHTML = ''; // очищаємо галерею перед новим пошуком
+  loader.style.display = 'block'; // показуємо лоадер
   searchImages(query)
     .then(images => {
-      loader.style.display = 'none'; // Ховаємо лоадер після отримання результатів
-
+      loader.style.display = 'none'; // ховаємо лоадер після отримання результатів
       if (images.length === 0) {
         showNoResultsMessage('Sorry, there are no images matching your search query. Please try again!');
         return;
       }
-
       updateGallery(images);
     })
     .catch(error => {
@@ -38,6 +29,5 @@ form.addEventListener('submit', function (event) {
       showNoResultsMessage('Error fetching images. Please try again!');
       console.error('Помилка сервера:', error.message);
     });
-
   form.reset();
 });
